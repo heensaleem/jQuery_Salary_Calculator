55e4e33b693cbebbab57ec6b2589b8a080bff315
heensaleem <heenakouser440@gmail.com>
2019-04-14 09:19:10 -0500

    created an employees array

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-$(document).ready(calculatorIsReady);
+//out employees array
+let employees = [];
 
-function calculatorIsReady(){
-    console.log('salary calculator');
+$( document ).ready( readyNow );
+
+function addEmployee(){
+ console.log('in addEmployees');
+ //get user input
+ //create a new object literal with the user input
+    const employee = {
+        firstName: $( '#firstNameIn' ).val(),
+        lastName :$( '#lastNameIn' ).val(),
+        idNumber : $( '#idNumberIn' ).val(),
+        jobTitle : $( '#jobTitleIn').val()
+        annualSalary : $( '#annualSalary').val()
+    }//end employee
+    //push the object into our employees array
+    (employees).push(employee);
+    //display employees to DOM
+    display employees( employees );
+}//end addEmployee
 
 
-}
-
-function addEmployeeInputs(){
-    let firstName = $()
-
-}
+ 
